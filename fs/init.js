e6cf19ea6bb768b35a4b7428041a958dd2beea28
sdavies <sdavies@thoughtworks.com>
2018-04-17 10:49:04 +0100

    Added temperature and humidity sensor - pin15

--- fs/init.js
+++ fs/init.js
@@ -5,12 +5,15 @@ load('api_mqtt.js');
 load('api_net.js');
 load('api_sys.js');
 load('api_timer.js');
+load('api_dht.js');
+
 
 let led = Cfg.get('pins.led');
 let button = Cfg.get('pins.button');
 let pirButton = 4;
 let soundButton = 27;
 let lightButton = 12;
+let tempButton = 15;
 
 
 let topic = '/devices/' + Cfg.get('device.id') + '/events';
@@ -18,8 +21,16 @@ let pirTopic = '/devices/' + Cfg.get('device.id') + '/pir/state';
 let buttonTopic = '/devices/' + Cfg.get('device.id') + '/button/state';
 let soundTopic = '/devices/' + Cfg.get('device.id') + '/sound/state';
 let lightTopic = '/devices/' + Cfg.get('device.id') + '/light/state';
+let tempTopic = '/devices/' + Cfg.get('device.id') + '/temp/state';
+let humidityTopic = '/devices/' + Cfg.get('device.id') + '/humidity/state';
+
+let roomId = "1234";
+
+
 
 print('LED GPIO:', led, 'button GPIO:', button);
+// Initialize DHT library
+let dht = DHT.create(tempButton, DHT.DHT22);
 
 let getInfo = function() {
   return JSON.stringify({
@@ -55,6 +66,29 @@ GPIO.set_button_handler(lightButton, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, funct
   let message = 'lightDetected';
   let ok = MQTT.pub(lightTopic, message, 1);
   print('LIGHT Published:', ok, lightTopic, '->', message);
+}, null);
+
+// This function reads data from the DHT sensor every 5 seconds
+Timer.set(5000 /* milliseconds */, Timer.REPEAT, function() {
+  let t = dht.getTemp();
+  let h = dht.getHumidity();
+
+  if (isNaN(h) || isNaN(t)) {
+    print('Failed to read data from temp sensor');
+    return;
+  }
+
+  print('Temperature:', t, '*C');
+  print('Humidity:', h, '%');
+  let tempMessage = JSON.stringify({status: t, room: roomId});
+  let humMessage = JSON.stringify({status: h, room: roomId});
+
+  let tok = MQTT.pub(tempTopic, tempMessage, 1);
+  print('TEMP Published:', tok, tempTopic, '->', tempMessage);
+
+  let hok = MQTT.pub(humidityTopic, humMessage, 1);
+  print('HUMIDITY Published:', hok, humidityTopic, '->', humMessage);
+
 }, null);
 
 
